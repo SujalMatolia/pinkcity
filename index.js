ed2d7c2d375ebba84fdf12aeb4e62417259f0130
sujal matolia <matoliasujal@gmail>
2023-07-11 19:36:02 +0530

    port no updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 7000;
+const port = 8080;
 const path = require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
